e12b70ad0daeb96b296a5b6b99478520e8e6c420
rzisoso <rzisoso@163.com>
2021-07-19 11:21:15 +0800

    CreateStateBar_Cpp_Temp

---
+++ Source/Building/FortniteEnum.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "FortniteEnum.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDING_API UFortniteEnum : public UObject
+{
+	GENERATED_BODY()
+	
+};
